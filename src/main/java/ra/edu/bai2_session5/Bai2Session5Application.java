501befc4e2f74b8cc2798f9be556a80be886d935
thaominh2006 <dothaominh2401@gmail.com>
2026-05-20 20:16:28 +0700

    bai2

---
+++ src/main/java/ra/edu/bai2_session5/Bai2Session5Application.java
@@ -0,0 +1,13 @@
+package ra.edu.bai2_session5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai2Session5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bai2Session5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ra/edu/bai2_session5/Bai2Session5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.edu.bai2_session5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai2Session5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
